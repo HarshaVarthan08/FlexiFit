39efff069e86624dc01d7b821e586ff6fada92c3
HarshaVarthan08 <harshavarthan.g2004@gmail.com>
2024-07-28 23:04:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home.js';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Workouts from './Components/workouts';
+import GoalPage from './Components/GoalPage.js';
+import WorkoutCalendar from './Components/WorkoutCalendar.js';
+import Adwork from './Components/adwork.js';
+import Dashboard from './Components/Dashboard.js';
+import UserProfile from './Components/UserProfile.js';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fnt'>
+      <Router>
+        
+      
+        <Routes>
+
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/workouts" element={<Workouts/>} />
+          <Route path="/goal" element={<GoalPage/>} />
+          <Route path="/dashboard" element={<WorkoutCalendar/>} />
+          <Route path="/adwork" element={<Adwork/>} />
+          <Route path="/stats" element={<Dashboard/>}/>
+          <Route path="/prof" element={<UserProfile/>}/>
+   
+        </Routes>
+        
+      </Router>
     </div>
   );
 }
